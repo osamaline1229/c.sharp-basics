b247090e630ade8a0d642d289ec58372e94101b1
unknown <osama98138139@gmail.com>
2023-05-30 11:31:39 +0400

    basics of c#

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("******Name:osama******");
+        Console.WriteLine("daaaaaaaaaaaaaaaaaaaad");
+        Console.WriteLine("father Name: Abdullah");
+        Console.WriteLine("Date of Birth: 1/1/1999");
+        Console.WriteLine("Email: osama98138139@gmail.com");
+        Console.WriteLine("github username: Osamaline1229");
+        Console.ReadLine();
+    }
+}
