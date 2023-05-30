fc883beb2429454fcbffb193d1841adbc2a0a6a3
unknown <osama98138139@gmail.com>
2023-05-30 13:04:12 +0400

    swith fuction

--- Program.cs
+++ Program.cs
@@ -2,8 +2,24 @@
 {
     private static void Main(string[] args)
     {
-        int time = 50;
-        string result = (time == 50) ? "good moring" : "good evening";
-        Console.WriteLine(result);
+        int month = 3;
+        switch (month)
+        {
+            case 1:
+                Console.WriteLine("january");
+                break;
+            case 2:
+                Console.WriteLine("Febuary");
+                break;
+            case 3:
+                Console.WriteLine("march");
+                break;
+            case 4:
+                Console.WriteLine("April");
+                break;
+            case 5:
+                Console.WriteLine("june");
+                break;
+        }
     }
 }
